cf99bc467d3ff81d47e490ebc83adeaf8e90577f
owend23 <57540794+owend23@users.noreply.github.com>
2021-05-19 10:33:42 -0400

    Update settings.py

--- src/project/settings.py
+++ src/project/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
+    'blog.apps.BlogConfig',
 
     'redactor',
     'easy_thumbnails',
